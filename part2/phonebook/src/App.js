1d1b8072e71c8545883ba9ba152c2fe9bcd3c304
Bethany Pietroniro <bethanypietroniro@TofuKimchiNoodleSoup.lan>
2023-06-27 12:58:15 -0400

    part2b

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,100 @@
+import { useState } from 'react'
+
+const Header = ({ text }) => <h1>{text}</h1>;
+
+const Person = ({ person }) => {
+  return (
+    <div>{person.name} {person.number}</div>
+  );
+};
+
+const Display = ({ list }) => {
+  return (
+    <>
+      {list.map(person => <Person key={person.name} person={person} />)}
+    </>
+  );
+};
+
+const InputField = ({ text, value, handler }) => {
+  return (
+    <div>
+      {text} <input value={value} onChange={handler} />
+    </div>
+  );
+};
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-1234567'}
+  ]) 
+  const [allNames, setAllNames] = useState({ 'Arto Hellas': true });
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+  const [query, setQuery] = useState('');
+
+  const addName = (event) => {
+    event.preventDefault();
+    if (allNames[newName]) {
+      alert(`${newName} already has an entry in the phonebook.`);
+      return
+    }
+
+    const newPerson = {
+      name: newName,
+      number: newNumber,
+    };
+    const allNamesUpdated = { ...allNames };
+    allNamesUpdated[newName] = true;
+
+    setPersons(persons.concat(newPerson));
+    setAllNames(allNamesUpdated);
+    setNewName('');
+    setNewNumber('');
+  };
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
+  
+  const handleQueryChange = (event) => {
+    setQuery(event.target.value);
+  };
+
+  return (
+    <div>
+      <Header text='Phonebook' />
+      <InputField 
+        text='filter shown with' 
+        value={query} 
+        handler={handleQueryChange}
+      />
+      <Header text='Add new' />
+      <form onSubmit={addName}>
+        <InputField 
+          text='name:'
+          value={newName} 
+          handler={handleNameChange}
+        />
+        <InputField 
+          text='number:'
+          value={newNumber} 
+          handler={handleNumberChange}
+        />
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <Header text='Numbers' />
+      <Display list={query ? persons.filter(person => {
+        return person.name.toLowerCase().includes(query.toLowerCase())
+      }) : persons } />
+    </div>
+  )
+}
+
+export default App
